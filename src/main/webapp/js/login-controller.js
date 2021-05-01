cf1a767aa66b909b8e8d0a6c7d681c972f45642a
pkt77 <parkerkt77@gmail.com>
2021-04-30 23:01:19 -0400

    Make login script work

--- src/main/webapp/js/login-controller.js
+++ src/main/webapp/js/login-controller.js
@@ -1,44 +1,32 @@
-document.getElementById('loginForm').addEventListener('submit', (event) => {
-        event.preventDefault();
-        console.log("logging in");
-    }
-);
-
 document.body.style.backgroundImage = "url('/Pictures/BackgroundPic.jfif')";
 
-function getValue() {
-    let customerName = document.getElementById("username").value;
-    let customerPassword = document.getElementById("password").value;
-    //console.log("user name is "+customerName);
-    //console.log("user password is "+customerPassword);
-    let xhttp = new XMLHttpRequest();
-    let basePokeUrl = "https://pokeapi.co/api/v2/pokemon/"
-    xhttp.open("GET", basePokeUrl + customerName);
-    xhttp.send();
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+        event.preventDefault();
 
+        let username = event.target['username'].value;
+        let password = event.target['password'].value;
 
-    xhttp.onreadystatechange = function () {
-        console.log("Changing ready state " + xhttp.readyState);
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-
-            let customerObject = JSON.parse(xhttp.responseText);
-            randomFunction(customerObject, customerName, customerPassword);
-
+        if (!username || !password) {
+            document.getElementById("msg").innerHTML = "Please fill out all the fields";
+            return;
         }
-    }
-}
 
-function randomFunction(cusObject, userName, password) {
-    //console.log("Order is "+cusObject.order);
-    //console.log("Name is "+cusObject.name);
-    if (cusObject.name === userName && cusObject.order == password) {
-        console.log("Milse");
-        var opened = window.open("welcome.html");
-        //opened.document.write("Your HTML here");
-    } else {
-        console.log("Mile Nai");
+        let xhttp = new XMLHttpRequest();
 
-        var opened = window.open("FailedLogIn.html");
+        xhttp.open("POST", "/api/login");
+
+        console.log("Changing ready state " + xhttp.readyState);
+        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState === 4) {
+                console.log(xhttp.status);
+            }
+        }
+
+        xhttp.send([
+            encodeURIComponent("username") + '=' + encodeURIComponent(username),
+            encodeURIComponent("password") + '=' + encodeURIComponent(password)
+        ].join('&'));
     }
-}
+);
