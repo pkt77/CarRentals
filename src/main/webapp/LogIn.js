476048ad47ceb73f5a61b4f9a550cc7d164a4248
AbdullahHady <hasanalif074@gmail.com>
2021-04-30 22:23:28 -0400

    Modified Login Page

--- src/main/webapp/LogIn.js
+++ src/main/webapp/LogIn.js
@@ -5,8 +5,6 @@ registerForm.addEventListener('submit',(Event)=>
         }
         );
 
-
-
 window.onload=function()
 {
     
@@ -19,49 +17,58 @@ function getValue()
     
     let customerName=document.getElementById("username").value;
     let customerPassword=document.getElementById("password").value;
-    //console.log("user name is "+customerName);
-    //console.log("user password is "+customerPassword);
-    let xhttp= new XMLHttpRequest();
-    let basePokeUrl="https://pokeapi.co/api/v2/pokemon/"
-    xhttp.open("GET",basePokeUrl+customerName);
-    xhttp.send();
-    
-
-    
-    xhttp.onreadystatechange=function()
+    if(!customerName || !customerPassword)
     {
-        console.log("Changing ready state "+xhttp.readyState);
-        if(xhttp.readyState==4 && xhttp.status==200)
-        {
-
-
-            let customerObject= JSON.parse(xhttp.responseText);
-            randomFunction(customerObject,customerName,customerPassword);
-        
-        }
-    }
-   
-
-}
-function randomFunction(cusObject,userName,password)
-
-{
-    //console.log("Order is "+cusObject.order);
-    //console.log("Name is "+cusObject.name);
-    if(cusObject.name===userName && cusObject.order==password)
-    {
-        console.log("Milse");
-        var opened = window.open("WelcomePage.html");
-        //opened.document.write("Your HTML here");
+        document.getElementById("msg").innerHTML="Please fill out all the fields";
     }
     else
     {
-        console.log("Mile Nai");
+            let customerObject={
+            username : customerName,
+            password :customerPassword
+        };
+        console.log("Customers username is "+customerObject.userName);
+        console.log("Customers password is "+customerObject.password);
+        
+        let xhttp= new XMLHttpRequest();
+        let jsonString="";
+        let url="http://localhost:8080/CarRentals/api/login"
+        //let url="https://pokeapi.co/api/v2/pokemon/"
 
-        var opened = window.open("FailedLogIn.html");
+        xhttp.open("POST",url);
+        //console.log("Check");
+        
+        
+    
+        console.log("Changing ready state "+xhttp.readyState);
+        xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        //xhttp.setRequestHeader("username",customerName);
+        //xhttp.setRequestHeader("password",customerPassword);
+
+
+        xhttp.onreadystatechange=function()
+        {
+            console.log("Changing ready state "+xhttp.readyState);
+            if(xhttp.readyState==4 && xhttp.status==200)
+            {
+    
+    
+                jsonString= JSON.stringify(customerObject);
+                console.log("Done");
+                
+                
+            
+            }
+            
+           
+        }
+        xhttp.send(customerObject);
         
 
     }
+    
+   
+
 }
 
 
