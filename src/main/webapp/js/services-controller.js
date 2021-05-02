a0f3d020cf118fd2d60e53085da9b0187933078f
pkt77 <parkerkt77@gmail.com>
2021-05-02 01:42:53 -0400

    Finish reservation form

--- src/main/webapp/js/services-controller.js
+++ src/main/webapp/js/services-controller.js
@@ -1,125 +1,169 @@
-let display = "";
-let display2 = "";
-let display3 = "";
-let flag = 0;
-document.getElementById("nextButton").addEventListener("click", displayNextVehicles);
-document.getElementById("previousButton").addEventListener("click", displayPreviousVehicles);
+document.body.style.backgroundImage = null;
 
+// Init Google Maps
+var map;
+var googleMaps;
 
-function displayVehicles() {
-    if (flag == 0) {
-        console.log("Checking0");
-        let xhttp = new XMLHttpRequest();
+if (googleMaps) {
+    initMap();
+} else {
+    googleMaps = document.createElement('script');
 
-        xhttp.open("GET", "/api/vehicles");
-        xhttp.send();
-        console.log(xhttp.readyState);
-        xhttp.onreadystatechange = function () {
+    googleMaps.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBBaHm8Z3C0bG3EHOPjpZk-68nItlTqEWY&callback=initMap";
+    document.body.appendChild(googleMaps);
+}
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 35.6134095, lng: -85.7001584},
+        zoom: 4
+    });
 
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                console.log("value of flag in if is  " + flag);
-                let vehicleObject = JSON.parse(xhttp.responseText);
-                console.log("Checking1");
+    var pReq = new XMLHttpRequest();
 
-                console.log("Checking");
-                let imageName = document.getElementById("myImg");
-                imageName.setAttribute("src", vehicleObject[flag].image);
+    pReq.onreadystatechange = function () {
+        if (pReq.readyState === 4 && pReq.status === 200) {
+            providers = JSON.parse(pReq.responseText);
 
-                display =
-                    '<p> VIN: ' + vehicleObject[flag].vin + '</p>' +
-                    '<p> Style: ' + vehicleObject[flag].style + '</p>' +
-                    '<p> MODEL: ' + vehicleObject[flag].model + '</p>' +
-                    '<p> MAKE: ' + vehicleObject[flag].make + '</p>' +
-                    '<p> MILEAGE: ' + vehicleObject[flag].mileage + '</p>' +
-                    '<p> RENT: ' + vehicleObject[flag].dailyCost + '</p>'
-                ;
+            for (var provider of providers) {
+                var mark = new google.maps.Marker({
+                    position: {lat: provider['lat'], lng: provider['longitude']},
+                    map: map
+                });
 
-                document.getElementById("myDiv").innerHTML = display;
+                google.maps.event.addListener(mark, 'click', function () {
+                    providerId = provider['id'];
+
+                    company.innerHTML = "Company: " + provider['company'];
+                    address.innerHTML = "Address: " + provider['street'] + " " + provider['city'] + ", " + provider['state'];
+                });
             }
-
-
         }
     }
 
-
+    pReq.open("GET", "/api/providers");
+    pReq.send();
 }
 
-function displayNextVehicles() {
-    if (flag < 12) {
-        flag++;
-        console.log("Checking0");
-        let xhttp = new XMLHttpRequest();
+// Init selection elements
+var company = document.getElementById('company');
+var address = document.getElementById('address');
 
-        xhttp.open("GET", "/api/vehicles");
-        xhttp.send();
-        console.log(xhttp.readyState);
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                console.log("value of flag in Next function  is  " + flag);
-                let vehicleObject = JSON.parse(xhttp.responseText);
-                console.log("Checking1");
+var providers;
+var providerId = -1;
 
-                console.log("Checking");
-                let imageName = document.getElementById("myImg");
-                imageName.setAttribute("src", vehicleObject[flag].image);
+var image = document.getElementById('image');
+var vin = document.getElementById('vin');
+var style = document.getElementById('style');
+var model = document.getElementById('model');
+var make = document.getElementById('make');
+var mileage = document.getElementById('mileage');
+var cost = document.getElementById('cost');
 
-                display2 =
-                    '<p> VIN: ' + vehicleObject[flag].vin + '</p>' +
-                    '<p> Style: ' + vehicleObject[flag].style + '</p>' +
-                    '<p> MODEL: ' + vehicleObject[flag].model + '</p>' +
-                    '<p> MAKE: ' + vehicleObject[flag].make + '</p>' +
-                    '<p> MILEAGE: ' + vehicleObject[flag].mileage + '</p>' +
-                    '<p> RENT: ' + vehicleObject[flag].dailyCost + '</p>'
-                ;
+var vehicles;
+var vehicleIndex = 0;
 
-                document.getElementById("myDiv").innerHTML = display2;
+// Fetch vehicle data
+var vReq = new XMLHttpRequest();
 
-            }
+vReq.onreadystatechange = function () {
+    if (vReq.readyState === 4 && vReq.status === 200) {
+        vehicles = JSON.parse(vReq.responseText);
 
+        displayVehicle(vehicleIndex);
+    }
+}
 
+vReq.open("GET", "/api/vehicles");
+vReq.send();
+
+function displayVehicle(index) {
+    if (index >= vehicles.length) {
+        vehicleIndex = 0;
+    } else if (index < 0) {
+        vehicleIndex = vehicles.length - 1;
+    } else {
+        vehicleIndex = index;
+    }
+
+    var vehicle = vehicles[vehicleIndex];
+
+    image.src = vehicle['image'];
+    vin.innerHTML = "VIN: " + vehicle['vin'];
+    style.innerHTML = "Style: " + vehicle['style'];
+    model.innerHTML = "Model: " + vehicle['model'];
+    make.innerHTML = "Make: " + vehicle['make'];
+    mileage.innerHTML = "Mileage: " + vehicle['mileage'];
+    cost.innerHTML = "Cost Per Day: $" + vehicle['dailyCost'];
+
+    updateTotal();
+}
+
+// Submission data
+var form = document.getElementById('reserve-form');
+var total = document.getElementById('total');
+
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+var tomorrow = new Date();
+
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+form['pickup'].min = tomorrow.getFullYear() + "-"
+    + (tomorrow.getMonth() < 9 ? "0" : "") + (tomorrow.getMonth() + 1) + "-"
+    + (tomorrow.getDate() < 10 ? "0" : "") + tomorrow.getDate();
+
+form['returned'].min = tomorrow.getFullYear() + "-"
+    + (tomorrow.getMonth() < 9 ? "0" : "") + (tomorrow.getMonth() + 1) + "-"
+    + (tomorrow.getDate() < 9 ? "0" : "") + (tomorrow.getDate() + 1);
+
+function updateTotal() {
+    var amount = vehicles[vehicleIndex]['dailyCost'];
+    var pickup = new Date(form['pickup'].value);
+    var returned = new Date(form['returned'].value);
+
+    if (!isNaN(pickup) && !isNaN(returned)) {
+        var diff = returned.getTime() - pickup.getTime();
+
+        amount *= diff / (1000 * 3600 * 24);
+    }
+
+    amount += Number(form['seats'].value) * 9.99;
+
+    if (form['gps'].checked) {
+        amount += 13.99;
+    }
+
+    total.innerHTML = "Total: " + formatter.format(amount);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (providerId === -1) {
+        alert("Please select a pickup location");
+        return;
+    }
+
+    var reserve = new XMLHttpRequest();
+
+    reserve.onreadystatechange = function () {
+        if (reserve.readyState === 4) {
+            console.log(reserve.status);
         }
     }
 
-
-}
-
-function displayPreviousVehicles() {
-    if (flag > 0) {
-        flag--;
-        console.log("Flag value inside previous function is " + flag);
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.open("GET", "/api/vehicles");
-        xhttp.send();
-        console.log(xhttp.readyState);
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                console.log("value of flag in if is  " + flag);
-                let vehicleObject = JSON.parse(xhttp.responseText);
-                console.log("Checking1");
-
-                console.log("Checking");
-                let imageName = document.getElementById("myImg");
-                imageName.setAttribute("src", vehicleObject[flag].image);
-
-                display3 =
-                    '<p> VIN: ' + vehicleObject[flag].vin + '</p>' +
-                    '<p> Style: ' + vehicleObject[flag].style + '</p>' +
-                    '<p> MODEL: ' + vehicleObject[flag].model + '</p>' +
-                    '<p> MAKE: ' + vehicleObject[flag].make + '</p>' +
-                    '<p> MILEAGE: ' + vehicleObject[flag].mileage + '</p>' +
-                    '<p> RENT: ' + vehicleObject[flag].dailyCost + '</p>';
-
-                document.getElementById("myDiv").innerHTML = display3;
-
-            }
-
-
-        }
-    }
-
-
-}
-
-displayVehicles();
+    reserve.open("POST", "/api/reservation");
+    reserve.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    reserve.send([
+        encodeURIComponent("location") + '=' + encodeURIComponent(providerId),
+        encodeURIComponent("vin") + '=' + encodeURIComponent(vehicles[vehicleIndex]['vin']),
+        encodeURIComponent("pickup") + '=' + encodeURIComponent(form['pickup'].value),
+        encodeURIComponent("returned") + '=' + encodeURIComponent(form['returned'].value),
+        encodeURIComponent("seats") + '=' + encodeURIComponent(form['seats'].value),
+        encodeURIComponent("gps") + '=' + encodeURIComponent(form['gps'].checked)
+    ].join('&'));
+});
